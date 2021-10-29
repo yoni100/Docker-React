cf94b57894b9128004ae420a49df16c76ee67794
yoni100 <yonatanforex@gmail.com>
2021-10-29 16:32:41 +0300

    added travis file 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bye there!!
+          bye there!!!
         </p>
         <a
           className="App-link"
